e5bf7706dcb6a0be8a54eb7a9f0fea481032d1c4
Kevin <kevin@nedellec.eu>
2021-12-29 13:34:49 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'black': '#151515',
+      'white': 'FFFFFF',
+      'gold': 'D5966C',
+      'gray': '444444'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
